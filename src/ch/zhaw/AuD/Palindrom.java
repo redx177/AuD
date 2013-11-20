30c1fd46a05fc27f1b22d8fe46d9f04ebcf1794b
soultemptation <red_x@hotmail.com>
2013-11-20 18:57:31 +0100

    Hanoi long

--- src/ch/zhaw/AuD/Palindrom.java
+++ src/ch/zhaw/AuD/Palindrom.java
@@ -5,7 +5,7 @@ public class Palindrom {
 	public static boolean IsPalindrom(String s) {
 		int length = s.length();
 		if (length == 0 || length == 1) return true;
-		if (s.substring(0,1).equals(s.substring(length-1,length))) return IsPalindrom(s.substring(1, length-1));
-		return false;
+		if (!s.substring(0,1).equals(s.substring(length-1,length))) return false;
+		return IsPalindrom(s.substring(1, length-1));
 	}
 }
